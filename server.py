50000faa7cbdffa7f8725d84a69b2517a45694be
Cameron2310 <wilke7566@gmail.com>
2025-05-07 20:22:28 -0400

    added parallel implementation and timing to test

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
+import argparse
 import socket
+import threading
 from routing import handle_path
 from http_message import Request
 
 
-def handle_request(client_sock, request_id: int):
-    data = client_sock.recv(1024)
-    print("\n\ndata ------> ", data)
-    request = Request(data)
-    response = handle_path(request)
-    print(f"\n\nresponse to request {request_id}\n", response)
-    client_sock.sendall(response)
-    
+def handle_request(client_sock, request_id: int, is_parallel):
+    try:
+        data = client_sock.recv(1024)
+        print("\n\ndata ------> ", data)
+        request = Request(data)
 
-def start_server(port: int, max_connections: int):
+        response = handle_path(request)
+        print(f"\n\nresponse to request {request_id}\n", response)
+        client_sock.sendall(response)
+
+    finally:
+        if is_parallel:
+            client_sock.close()
+
+
+def start_server(port: int, max_connections: int, is_parallel: bool):
     request_count = -1
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
@@ -24,12 +32,26 @@ def start_server(port: int, max_connections: int):
             request_count += 1
 
             try:
-                handle_request(client_sock, request_count)
+                if is_parallel:
+                    t = threading.Thread(target=handle_request, args=((client_sock, request_count, is_parallel)))
+                    t.daemon = True
+                    t.start()
+                
+                else:
+                    print("serial implementation running...")
+                    handle_request(client_sock, request_count, is_parallel)
 
             finally:
-                print("closing connection...")
-                client_sock.shutdown(socket.SHUT_WR)
-                client_sock.close()
+                if not is_parallel:
+                    print("shutting down connection...")
+                    client_sock.shutdown(socket.SHUT_WR)
+                    client_sock.close()
 
-                
-start_server(8000, 5)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", help="run each request on it's own thread", action="store_true")
+    args = parser.parse_args()
+    
+    is_parallel = True if args.c else False
+    start_server(8000, 5, is_parallel)
--- tests/concurrent_requests.py
+++ tests/concurrent_requests.py
@@ -2,26 +2,34 @@ import random
 import requests
 import string
 import threading
+import time
+import concurrent.futures
 
 
 def generate_body(length: int):
     return "".join([random.choice(string.ascii_letters + string.digits) for _ in range(length)])
 
 
-def make_get_request(t_id: int):
-    body = generate_body(128)
-    print(f"\n\ngenerated body for request {t_id}: {body}")
+def generate_success_percentage(total: int, success_count: int):
+    return (success_count / total) * 100
+
+
+# def make_get_request(t_id: int):
+def make_get_request(request_id, body=None):
+    if not body:
+        body = generate_body(128)
     
     while True:
         n_retries = 0
         
         try:
             response = requests.get("http://localhost:8000/echo", data=body)
-            print(f"request {t_id}: {response.text}\n")
-            break
+            print(f"request {request_id}: {response.text}\n")
+            
+            return response.text
+
         except Exception as e:
-            print("Exception ", e)
-            print("\n\nhandling exception\n\n")
+            print(f"Exception {e}\nRetrying connection...")
 
             n_retries += 1
             if n_retries > 5:
@@ -41,9 +49,30 @@ def run_concurrent_test(num_threads: int):
         t.join()
 
 
-def run_basic_test_w_body():
-    for i in range(50):
-        make_get_request(i)
+def run_concurrent_test_2():
+    body = [generate_body(128) for _ in range(10)]
 
-# run_basic_test_w_body()
-run_concurrent_test(10)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor: # optimally defined number of threads
+        res = [executor.submit(make_get_request, b) for b in body]
+        concurrent.futures.wait(res)
+
+
+def run_basic_test_w_body():
+    responses = []
+    count = 0
+
+    start = time.time()
+    for i in range(500):
+        responses.append(make_get_request(i))
+
+    end = time.time()
+
+    for r in responses:
+        if r:
+            count += 1
+
+    print(f"{end - start} secs")
+    print(f"Successful response percentage: {generate_success_percentage(len(responses), count)}%")
+
+run_basic_test_w_body()
+# run_concurrent_test(10)
