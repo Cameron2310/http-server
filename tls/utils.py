1baf192ecfb121aa4d913c7fe18c8aafbfeb6b3b
Cameron2310 <wilke7566@gmail.com>
2025-05-12 18:34:49 -0400

    Updated client_hello to output bytes

--- tls/utils.py
+++ tls/utils.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
+from typing import List
 
 KeyPair = namedtuple("KeyPair", ["private_key", "public_key"])
 
@@ -9,3 +10,12 @@ def generate_key_pair():
     public_key = private_key.public_key()
 
     return KeyPair(private_key, public_key) 
+
+
+def create_extension(id: int, contents: List[int]) -> List[int]:
+    id_bytes = [int(i) for i in id.to_bytes(2, "big")]
+    content_bytes = [int(i) for i in (len(contents)).to_bytes(2, "big")]
+    extension = [*id_bytes, *content_bytes, *contents]
+    print("extension ----> ", extension)
+
+    return extension
